bd8e14998f11083bb4dbea8a5b920519ba888bb8
kirstinlyon <kirstin@mltwelve.com>
2025-04-09 21:45:12 +0200

    setting environment for Airtable API key

--- Scripts/airtable_and_r_github.R
+++ Scripts/airtable_and_r_github.R
@@ -16,7 +16,7 @@ PERSONAL_ACCESS_TOKEN <- Sys.getenv("AIRTABLE_PAT")  # personal access token sto
 BASE_ID <- Sys.getenv("AIRTABLE_BASE_ID")  # base id of table in Airtable
 
 # Set Airtable API key in environment
-#Sys.setenv(AIRTABLE_API_KEY = PERSONAL_ACCESS_TOKEN)
+Sys.setenv(AIRTABLE_API_KEY = PERSONAL_ACCESS_TOKEN)
 
 # Validate API key
 if (PERSONAL_ACCESS_TOKEN == "") {
